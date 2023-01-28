6eb495d7d3cf0217e29648a9cda0b1a5be3c5dfb
Stephane_tcha <stephanetcha@gmail.com>
2023-01-28 15:50:06 +0100

    try-catch-ressources

--- MonProgramme.java
+++ MonProgramme.java
@@ -1,5 +1,14 @@
+import java.io.FileReader;
+import java.io.IOException;
 public class MonProgramme {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try (FileReader fr = new FileReader("fichierInexistant.txt")){
+            char [] a = new char[50];
+            fr.read(a);
+            for(char c : a)
+                System.out.print(c);
+        } catch(IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
