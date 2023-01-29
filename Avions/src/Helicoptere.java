bcd3c7e3ce4906d0caced431d3c520c0521ac03a
Stephane_tcha <stephanetcha@gmail.com>
2023-01-29 15:19:44 +0100

    collections

--- Avions/src/Helicoptere.java
+++ Avions/src/Helicoptere.java
@@ -1,4 +1,16 @@
 
 public class Helicoptere extends Aeronef {
-
+	public Helicoptere(int numeroImmatriculation, String nomModele, int nombrePlacesABord) {
+		super(numeroImmatriculation, nomModele, nombrePlacesABord);
+	}
+	@Override
+	public void decoller() {
+		System.out.println("L'hélicoptère " + this.getNomModele() + ", immatriculé " + this.getNumeroImmatriculation() + ", fait tourner son hélice.");
+		System.out.println("L'hélicoptère " + this.getNomModele() + ", immatriculé " + this.getNumeroImmatriculation() + ",  monte et quitte le sol.");
+	}
+	@Override
+	public void atterrir() {
+		System.out.println("L'hélicoptère " + this.getNomModele() + ", immatriculé " + this.getNumeroImmatriculation() + ", descend jusqu'au sol.");
+		System.out.println("L'hélicoptère " + this.getNomModele() + ", immatriculé " + this.getNumeroImmatriculation() + ", arrête de faire tourner son hélice.");
+	}
 }
--- ecriture.java
+++ ecriture.java
@@ -1,4 +1,9 @@
 public class ecriture {
- 
+    Map<String, Integer> map = new HashMap<>();
+    map.put("first", 10);
+    map.put("second", 30);
+    map.put("third", 50);
+    map.get("third");
+
 }
 
