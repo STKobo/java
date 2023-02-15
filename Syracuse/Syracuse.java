ef4e1aa858a0b0f6bedb795d0035cf978d6c3a35
Stephane_tcha <stephanetcha@gmail.com>
2023-02-15 19:15:34 +0100

    algorithme de syracuse

---
+++ Syracuse/Syracuse.java
@@ -0,0 +1,30 @@
+package Syracuse;
+
+import java.util.Scanner;
+
+public class Syracuse {
+
+    public static void main( String[] args){
+
+    }
+
+    public static void syracuse(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Entrez votre entier : ");
+        int entier = sc.nextInt();
+        int compteur = 0;
+        if(entier == 1 || entier <= 0){
+            System.out.println("compteur : 0");
+        } else {
+            while(entier != 1){
+                compteur++;
+                if(entier % 2 == 0){
+                    entier = entier / 2;
+                } else {
+                    entier = 3*entier+1;
+                }
+            }
+            System.out.println("le compteur est : " + compteur);
+        }
+    }
+}
