839d00354c7e8cf8d1c4d555a48f9c56cf60eaba
Stephane_tcha <stephanetcha@gmail.com>
2023-02-15 19:29:19 +0100

    haut saut

--- Syracuse/Syracuse.java
+++ Syracuse/Syracuse.java
@@ -13,18 +13,30 @@ public class Syracuse {
         System.out.println("Entrez votre entier : ");
         int entier = sc.nextInt();
         int compteur = 0;
+        int compteurHautSaut = 0;
+        int compteurHautSautTmp = 0;
+
         if(entier == 1 || entier <= 0){
             System.out.println("compteur : 0");
         } else {
             while(entier != 1){
                 compteur++;
+
+                if(entier >= 10){
+                    compteurHautSautTmp ++;
+                } else {
+                    if(compteurHautSautTmp > compteurHautSaut) {
+                        compteurHautSaut = compteurHautSautTmp;
+                    }
+                }
+
                 if(entier % 2 == 0){
                     entier = entier / 2;
                 } else {
                     entier = 3*entier+1;
                 }
             }
-            System.out.println("le compteur est : " + compteur);
+            System.out.println("le compteur est : " + compteur + " celui des hauts sauts est : " + compteurHautSaut);
         }
     }
 }
